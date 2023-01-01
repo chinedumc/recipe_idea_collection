b5aca4cf71317dc7b62d853c7952b626876a0e58
chibokamc <chibokachinedum@gmail.com>
2023-01-01 22:19:12 +0100

    Added Routes with R-Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleRecipe from "./pages/SingleRecipe";
+import Error from "./pages/Error";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SharedLayout from "./pages/SharedLayout";
 
 function App() {
-  return (
-    <div>
-      hello
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<SharedLayout />}>
+					<Route index element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/recipe/:id" element={<SingleRecipe />} />
+					<Route path="*" element={<Error />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/pages/SingleRecipe.js
@@ -0,0 +1,9 @@
+const SingleRecipe = () => {
+	return (
+		<div>
+			<h1>SingleRecipe</h1>
+		</div>
+	);
+};
+
+export default SingleRecipe;
